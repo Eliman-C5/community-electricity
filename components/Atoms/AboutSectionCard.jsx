94cc55b2458bd4377ee545d70f6c5c2d4f703818
eli castellano <eliman1926@gmail.com>
2022-11-25 11:02:40 -0400

    Resolviendo problema con Vercel

--- components/Atoms/AboutSectionCard.jsx
+++ components/Atoms/AboutSectionCard.jsx
@@ -5,7 +5,7 @@ export const AboutSectionCard = ({title, text, btn, src}) => {
     <div className='w-full flex flex-col-reverse md:flex-row items-center max-w-[1135px] mr-auto h-full md:h-[470px] bg-[#F2F2F2]'>
       <div className="w-full md:w-1/2 px-[1.5rem] py-[1.5rem] md:py-[3rem] flex flex-col gap-4">
         <h2 className='title-mobile md:title text-black'>{title}</h2>
-        <p className='text-black'>{text}</p>
+        <p className='text-black '>{text}</p>
         <span className='text-black font-semibold'>{btn}</span>
       </div>
       <div className="w-full h-[250px] md:w-1/2 md:h-full order-1">
