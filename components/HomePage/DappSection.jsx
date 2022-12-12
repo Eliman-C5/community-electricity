d71eddddb6c543c4ef2019dc5e37e82137462728
eli castellano <eliman1926@gmail.com>
2022-12-12 10:37:58 -0400

    Cambio de imagen en dappSection

--- components/HomePage/DappSection.jsx
+++ components/HomePage/DappSection.jsx
@@ -18,21 +18,9 @@ export const DappSection = () => {
           <div className="flex items-center text-[28px] md:text-[40px] font-semibold mb-5">
           The <img src="/images/glu.png" alt="" className='h-[40px] md:h-[60px]' /> dapp
           </div>
-          {/* <img 
-            src="/images/glu dapp.png" 
-            alt="About Home" 
-            className='w-[180px] h-[44px] md:w-[252px] md:h-[56px] mt-[15px]' 
-          /> */}
           
-          <img src="/images/glu movil.png" alt="" className='hidden md:block' />
+          <img src="/images/glu movil.png" alt="" className='hidden md:block w-[380px] h-[280px]' />
           <img src="/images/glu movil-mobile.png" alt="" className='block md:hidden' />
-          
-          {/* <div className="flex">
-            <img src="/images/glu-movil-1.png" alt="" className='hidden md:block w-fit h-fit z-10' />
-            <img src="/images/glu-movil-2.png" alt="" className='hidden md:block h-fit md:w-fit relative -left-20 mt-28' />
-            <img src="/images/glu-movil-1.png" alt="" className='block md:hidden w-fit h-fit z-10' />
-            <img src="/images/glu-movil-2.png" alt="" className='block md:hidden h-fit md:w-fit relative -left-20 mt-28' />
-          </div> */}
           
           <p className='my-4 font-bold'>
           {
