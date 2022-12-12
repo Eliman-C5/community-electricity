c3fc40f78fb573fbb9226541b1df102721cdd561
eli castellano <eliman1926@gmail.com>
2022-12-12 09:37:00 -0400

    Cambios en imagenes en dappSection

--- components/HomePage/DappSection.jsx
+++ components/HomePage/DappSection.jsx
@@ -15,15 +15,24 @@ export const DappSection = () => {
       
         <div className="w-full md:w-1/2">
         
-          <img 
+          <div className="flex items-center text-[28px] md:text-[40px] font-semibold mb-5">
+          The <img src="/images/glu.png" alt="" className='h-[40px] md:h-[60px]' /> dapp
+          </div>
+          {/* <img 
             src="/images/glu dapp.png" 
             alt="About Home" 
             className='w-[180px] h-[44px] md:w-[252px] md:h-[56px] mt-[15px]' 
-          />
-          <div className="flex">
-            <img src="/images/glu-movil-1.png" alt="" className='w-[127px] md:w-fit md:h-fit z-10' />
-            <img src="/images/glu-movil-2.png" alt="" className='w-[240px] h-[121px] mt-24 md:h-fit md:w-fit relative -left-20 md:mt-28' />
-          </div>
+          /> */}
+          
+          <img src="/images/glu movil.png" alt="" className='hidden md:block' />
+          <img src="/images/glu movil-mobile.png" alt="" className='block md:hidden' />
+          
+          {/* <div className="flex">
+            <img src="/images/glu-movil-1.png" alt="" className='hidden md:block w-fit h-fit z-10' />
+            <img src="/images/glu-movil-2.png" alt="" className='hidden md:block h-fit md:w-fit relative -left-20 mt-28' />
+            <img src="/images/glu-movil-1.png" alt="" className='block md:hidden w-fit h-fit z-10' />
+            <img src="/images/glu-movil-2.png" alt="" className='block md:hidden h-fit md:w-fit relative -left-20 mt-28' />
+          </div> */}
           
           <p className='my-4 font-bold'>
           {
