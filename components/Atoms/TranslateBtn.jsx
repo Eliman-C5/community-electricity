1c0b172baca51352ecfa82bc987d567f1bda9048
eli castellano <eliman1926@gmail.com>
2022-12-15 10:55:37 -0400

    Alineación de textos en TranslateBtn

--- components/Atoms/TranslateBtn.jsx
+++ components/Atoms/TranslateBtn.jsx
@@ -7,7 +7,7 @@ export const TranslateBtn = ({style = ''}) => {
   const {spanishIsActive, setSpanishIsActive} = useContext(LanguageContext)
 
   return (
-    <div className={`flex gap-4 ${style}`}>
+    <div className={`flex gap-4 ${style} items-center`}>
       <span className='text-white sm:text-black text-[12px]'>EN</span>
         <label className="switch">
           <input type="checkbox" onClick={() => setSpanishIsActive(!spanishIsActive)} />
