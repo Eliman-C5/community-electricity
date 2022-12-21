656f829d0c1704bb1353233e7cbc2624d10238c1
eli castellano <eliman1926@gmail.com>
2022-12-21 09:37:25 -0400

    Cambiando size en imagenes de services en home

--- components/HomePage/servicesHome/ServiceTitle.jsx
+++ components/HomePage/servicesHome/ServiceTitle.jsx
@@ -12,8 +12,8 @@ export const ServiceTitle = ({ spanishIsActive, style }) => {
       </h2>
       
       <div className="flex my-[30px] md:my-[60px] relative justify-center">
-        <img src="/images/innovative-1.png" alt="" className='w-1/2 min-h-[120px]' />
-        <img src="/images/innovative-2.png" alt="" className='w-1/2 min-h-[120px]'/>
+        <img src="/images/innovative-1.png" alt="" className='w-1/2 min-h-[175px]' />
+        <img src="/images/innovative-2.png" alt="" className='w-1/2 min-h-[175px]'/>
         <img src="/images/glu logo.png" alt="" className='w-[70px] h-[60px] xl:w-[133px] xl:h-[124px] absolute bottom-0' />
       </div>
     </div>
