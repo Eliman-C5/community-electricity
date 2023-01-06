a7a0f34ed82ac0a2bccc76d5e5c3f081ba973364
eli castellano <eliman1926@gmail.com>
2023-01-06 18:06:58 -0400

    Cambios en sieze

--- components/HomePage/Dapp/TheGluTitle.jsx
+++ components/HomePage/Dapp/TheGluTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const TheGluTitle = ({title, align = 'justify-center'}) => {
   return (
     <div className={`flex items-center ${align} text-[28px] md:text-[40px] font-semibold mb-5`}>
-      <img src="/images/glu.png" alt="" className='h-[50px] md:h-auto mr-1' /> {title}
+      <img src="/images/glu.png" alt="" className='h-[45px] w-[52px] md:h-auto md:w-auto mr-1' /> {title}
     </div>
   )
 }
